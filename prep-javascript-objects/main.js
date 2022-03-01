b6df0fbf8db083333f0f5aba3e95fe114a2d6fc4
Nicolas Sturz <nicksturz92@gmail.com>
2022-03-01 20:03:01 +0000

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,15 @@
+var person = {
+  firstName: 'Nick',
+  lastName: 'Sturz',
+  hobby: 'playing elden ring',
+
+};
+
+var fullName = 'Nick' + ' ' + 'Sturz';
+console.log(fullName);
+
+person.job = 'hopefully coding';
+
+person.previousJob = 'fruit stand specialist';
+
+console.log(person);
